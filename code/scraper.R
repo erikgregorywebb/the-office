437fe63dd5343432b347a94e9ca00e1a35cda603
Erik <erikgregorywebb@gmail.com>
2021-12-23 02:36:02 -0500

    Create scraper.R

---
+++ code/scraper.R
@@ -0,0 +1,50 @@
+#library(robotstxt)
+#library(urltools)
+#robotstxt::get_robotstxt(urltools::domain("https://www.officequotes.net")) %>% cat()
+
+library(tidyverse)
+library(stringr)
+library(rvest)
+
+# episode links
+url = 'https://www.officequotes.net/'
+page = read_html(url)
+all_links = page %>% html_node('#text-6') %>% html_nodes('a') %>% html_attr('href')
+episode_links = paste('https://www.officequotes.net', all_links[str_detect(all_links, 'no')], sep = '')
+
+# lines
+datalist = list()
+counter = 1
+for (i in 1:length(episode_links)) {
+  tryCatch({
+    Sys.sleep(5)
+    print(episode_links[i])
+    
+    # season and episode number
+    r = str_extract(episode_links[i], '\\d-\\d\\d')
+    season = as.numeric(substr(r, 1, 1))
+    episode = as.numeric(substr(r, 3, 5))
+    
+    # episode scenes
+    page = read_html(episode_links[i])
+    scenes = page %>% html_nodes('.quote')
+    for (j in 1:length(scenes)) {
+      lines = scenes[j] %>% html_text2() %>% strsplit(., "\\n") %>% unlist()
+      temp = tibble(
+        season = season,
+        episode = episode,
+        scene = j,
+        text = lines
+      ) %>%
+        mutate(line = row_number())
+      datalist[[counter]] = temp
+      counter = counter + 1
+      print(paste(season, episode, j, sep = ' : '))
+    }
+  },
+  error = function(error_message) {
+    message(error_message)
+    return(NA)
+  })
+}
+raw = do.call(rbind, datalist)
